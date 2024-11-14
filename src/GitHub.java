0103c7757b7ef498dc7b6399e50ea079aecda2f7
ltdung8 <ledung1998hello@gmail.com>
2024-11-14 20:55:56 +0700

    GitHub1

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class GitHub {
+    public static void main(String[] args) {
+        System.out.println("DEMO GOD TO GITHUB");
+        System.out.println("Selenium");
+        System.out.println("learn GIT");
+        int a =1;
+        int b = 23333333;
+        System.out.println(a + b);
+        System.out.println(a*b);
+        System.out.println(a*b - 100000);
+        System.out.println("GIT HUB");
+    }
 }
