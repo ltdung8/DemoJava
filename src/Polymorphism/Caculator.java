6fc62dbadf9cb830b146c7605a86dd2aafbd9485
ltdung8 <143996456+ltdung8@users.noreply.github.com>
2024-12-08 19:34:00 +0700

    v1.2

--- src/Polymorphism/Caculator.java
+++ src/Polymorphism/Caculator.java
@@ -1,4 +1,8 @@
 package Polymorphism;
 
 public class Caculator {
+    public static void main(String[] args) {
+        System.out.println("TINH DA HINH");
+        System.out.println("Test 1.2");
+    }
 }
